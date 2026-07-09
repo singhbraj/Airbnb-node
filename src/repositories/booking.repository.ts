ed9669e8e9eb74e6c146e32694d3327eec1203ce
singhbraj <braj333singh@gmail.com>
2026-07-10 01:01:15 +0530

    Completed the booking repository

--- src/repositories/booking.repository.ts
+++ src/repositories/booking.repository.ts
@@ -7,3 +7,62 @@ export async function createBooking(bookingInput: Prisma.BookingCreateInput): Pr
     });
     return booking;
 }
+
+export async function createIdempotencyKey(key: string, bookingId: number) {
+
+    const idempotencyKey = await prisma.idempotencyKey.create({
+        data: {
+            key,
+            booking: {
+                connect: {
+                    id: bookingId
+                }
+            }
+
+        }
+    });
+    return idempotencyKey;
+}
+
+export async function GetIdempotencyKey(key: string) {
+    const idempotencyKey = await prisma.idempotencyKey.findUnique({
+        where: {
+            key
+        }
+    });
+    return idempotencyKey;
+}
+
+export async function getBookingById(id: number) {
+    const booking = await prisma.booking.findUnique({
+        where: {
+            id
+        }
+    });
+    return booking;
+}
+
+
+export async function confirmBooking(id: number) {
+    const booking = await prisma.booking.update({
+        where: {
+            id
+        },
+        data: {
+            status: "CONFIRMED"
+        }
+    });
+    return booking;
+}
+
+export async function finalizeIdempotencyKey(id: number) {
+    const idempotencyKey = await prisma.idempotencyKey.update({
+        where: {
+            id
+        },
+        data: {
+            finalized: true
+        }
+    });
+    return idempotencyKey;
+}
