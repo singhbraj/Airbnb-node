96a7ad17f4ae462be6e89d9a889b1968ed670ec2
singhbraj <braj333singh@gmail.com>
2026-07-12 20:52:36 +0530

    Added notification service

---
+++ NotificationService/src/config/index.ts
@@ -0,0 +1,25 @@
+// This file contains all the basic configuration logic for the app server to work
+import dotenv from 'dotenv';
+
+type ServerConfig = {
+    PORT: number,
+    REDIS_PORT?: number,
+    REDIS_HOST?: string,
+    MAIL_PASS?: string,
+    MAIL_USER?: string,
+}
+
+function loadEnv() {
+    dotenv.config();
+    console.log(`Environment variables loaded`);
+}
+
+loadEnv();
+
+export const serverConfig: ServerConfig = {
+    PORT: Number(process.env.PORT) || 3001,
+    REDIS_PORT: process.env.REDIS_PORT ? Number(process.env.REDIS_PORT) : 6379,
+    REDIS_HOST: process.env.REDIS_HOST || 'localhost',
+    MAIL_PASS: process.env.MAIL_PASS || '',
+    MAIL_USER: process.env.MAIL_USER || ''
+};
---
+++ NotificationService/src/processors/email.processor.ts
@@ -0,0 +1,43 @@
+import { Job, Worker } from "bullmq";
+import { NotificationDto } from "../dto/notification.dto";
+import { MAILER_QUEUE } from "../queues/mailer.queue";
+import { getRedisConnObject } from "../config/redis.config";
+import { MAILER_PAYLOAD } from "../producers/email.producer";
+import { renderMailTemplate } from "../templates/templates.handler";
+import { sendEmail } from "../services/mailer.service";
+import logger from "../config/logger.config";
+
+export const setupMailerWorker = () => {
+
+    const emailProcessor = new Worker<NotificationDto>(
+        MAILER_QUEUE, // Name of the queue
+        async (job: Job) => {
+
+            if(job.name !== MAILER_PAYLOAD) {
+                throw new Error("Invalid job name");
+            }
+
+            // call the service layer from here.
+            const payload = job.data;
+            console.log(`Processing email for: ${JSON.stringify(payload)}`);
+
+            const emailContent = await renderMailTemplate(payload.templateId, payload.params);
+
+            await sendEmail(payload.to, payload.subject, emailContent);
+
+            logger.info(`Email sent to ${payload.to} with subject "${payload.subject}"`);
+
+        }, // Process function
+        {
+            connection: getRedisConnObject()
+        }
+    )
+
+    emailProcessor.on("failed", () => {
+        console.error("Email processing failed");
+    });
+
+    emailProcessor.on("completed", () => {
+        console.log("Email processing completed successfully");
+    });
+}
