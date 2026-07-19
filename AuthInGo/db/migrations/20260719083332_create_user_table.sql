76dc3d519f74e1c54b4f395841c952688b119209
singhbraj <braj333singh@gmail.com>
2026-07-19 14:14:01 +0530

    Added goose for migrations

---
+++ AuthInGo/db/migrations/20260719083332_create_user_table.sql
@@ -0,0 +1,13 @@
+-- +goose Up
+
+CREATE TABLE users(
+   id SERIAL PRIMARY KEY,
+   username VARCHAR(255) NOT NULL,
+   email VARCHAR(255) NOT NULL,
+   password VARCHAR(255) NOT NULL,
+   created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
+   updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp
+);
+
+-- +goose Down
+DROP TABLE users;
--- AuthInGo/go.mod
+++ AuthInGo/go.mod
@@ -4,10 +4,8 @@ go 1.26.5
 
 require (
 	github.com/go-chi/chi/v5 v5.3.1
+	github.com/go-sql-driver/mysql v1.10.0
 	github.com/joho/godotenv v1.5.1
 )
 
-require (
-	filippo.io/edwards25519 v1.2.0 // indirect
-	github.com/go-sql-driver/mysql v1.10.0 // indirect
-)
+require filippo.io/edwards25519 v1.2.0 // indirect
